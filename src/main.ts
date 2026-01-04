6b8012c51cc3c27b27c541035d1c24b7fe47acb5
yubaobin <502275531@qq.com>
2026-01-04 08:46:08 +0800

    去掉百度统计

--- src/main.ts
+++ src/main.ts
@@ -20,14 +20,3 @@ async function bootstrap () {
 }
 
 bootstrap()
-
-const isProduction = import.meta.env.MODE === 'production'
-
-if (isProduction) {
-    const hm = document.createElement('script')
-    hm.src = 'https://hm.baidu.com/hm.js?29f479ad1a28ef790cdf0e48b3fbf4e8'
-    const s = document.getElementsByTagName('script')[0]
-    if (s && s.parentNode) {
-        s.parentNode.insertBefore(hm, s)
-    }
-}
